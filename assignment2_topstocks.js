89c73ecf8a7c984fbc1f9a8c533725c17e4c9273
Shahil Paudel <shahilpaudel@gmail.com>
2025-05-01 19:23:37 -0400

    Changes to topstocks.js

--- assignment2_topstocks.js
+++ assignment2_topstocks.js
@@ -3,6 +3,9 @@ async function getTop() {
     const api_url = "https://tradestie.com/api/v1/apps/reddit?date=2022-04-03";
     const response = await fetch(api_url);
     const data = await response.json();
+    // use for each to create table and use a const to hold index
+    // append every ticket, count, and sentiment in a for each loop at the end
+    // don't need multiple functions
     console.log(data);
     data.forEach((item) => item.sort((a, b) => b[1] - a[1]));
     console.log(data);
@@ -14,6 +17,28 @@ async function getTop() {
 async function populateTable() {
     topFive = await getTop();
     // loop through array of dictionaries, take element and append it to tbody
-    topFive.forEach((item) => )
+    var tbody = document.getElementById("tbody");
+    var table = document.getElementById("table");
+    topFive.forEach((item) => {
+        const row = document.createElement("tr");
+        const tickCell = document.createElement("td");
+        tickCell.textContent = item.ticker;
+        row.appendChild(tickCell);
+        const countCell = document.createElement("td");
+        countCell.textContent = item.no_of_comments;
+        row.appendChild(countCell);
+        const sentimentCell = document.createElement("td");
+        sentimentCell.textContent = item.sentiment;
+        row.appendChild(sentimentCell);
+        tbody.appendChild(row);
+    })
+    table.appendChild(tbody);
+    
+    // for (let i = 0; i < topFive.length; i++) {
+    //     var currentTick = topFive[i].ticker;
+    //     var currentCount = topFive[i].no_of_comments;
+    //     var currentSentiment = topFive[i].sentiment;
+
+    // }
 }
-document.getElementById("firstTick").innerHTML = `${top.ticker.value}`
+// document.getElementById("firstTick").innerHTML = `${top.ticker.value}`

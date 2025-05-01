c84283756192d1fc4a0d7220e862a035bbe1dc84
Shahil Paudel <shahilpaudel@gmail.com>
2025-05-01 18:15:29 -0400

    Stuck on populating the table

--- assignment2_topstocks.js
+++ assignment2_topstocks.js
@@ -1,7 +1,19 @@
-const api_url = "https://tradestie.com/api/v1/apps/reddit?date=2022-04-03";
 async function getTop() {
+    var sorted = []
+    const api_url = "https://tradestie.com/api/v1/apps/reddit?date=2022-04-03";
     const response = await fetch(api_url);
     const data = await response.json();
     console.log(data);
-    // for ()
+    data.forEach((item) => item.sort((a, b) => b[1] - a[1]));
+    console.log(data);
+    top = data.slice(0, 4);
+    console.log(top);
+    return top;
 }
+
+async function populateTable() {
+    topFive = await getTop();
+    // loop through array of dictionaries, take element and append it to tbody
+    topFive.forEach((item) => )
+}
+document.getElementById("firstTick").innerHTML = `${top.ticker.value}`
